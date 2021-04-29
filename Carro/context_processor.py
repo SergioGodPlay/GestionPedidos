2dfdc9ea16084c1a447b94cb4dc2748b1e0887cc
Sergio Sanchez <richardgodplay@gmail.com>
2021-04-29 18:18:58 -0500

    Agregada variable para almacenar precio de los productos y boton

---
+++ Carro/context_processor.py
@@ -0,0 +1,12 @@
+
+def importe_total_carro(request):
+
+    total = 150
+
+    if request.user.is_authenticated:
+
+        for key, value in request.session["carro"].items():
+
+            total = total+(float(value["precio"]) * value["cantidad"])
+
+    return {"importe_total_carro":total}
